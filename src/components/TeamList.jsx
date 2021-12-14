0daa1998c2bdb3f32068f12c21bd85f22099eb6e
Frank Hollywood <github@srvsync.com>
2021-12-14 15:49:45 -0800

    fixing CI issues

--- src/components/TeamList.jsx
+++ src/components/TeamList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { deleteTeamById, getTeams } from '../services/teams';
 
